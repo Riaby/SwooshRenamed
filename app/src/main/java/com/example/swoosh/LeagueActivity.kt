718bf9dfc9b9834cbf7eef9b68b5f00f5145d6f2
Anna Riabykh <anna.ryabykh@gmail.com>
2020-06-16 18:07:27 -0400

    added Desired Laguage screen

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,14 @@
 package com.example.swoosh
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class WelcomeActivity : AppCompatActivity() {
+
+class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_league)
     }
 }
+
